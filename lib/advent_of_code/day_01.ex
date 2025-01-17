907fd50d6f4f34c43830210a4c3fc856d9e5f926
Joshua Wootonn <joshuawootonn@gmail.com>
2025-01-16 21:24:41 -0500

    feat: day 1 2024

--- lib/advent_of_code/day_01.ex
+++ lib/advent_of_code/day_01.ex
@@ -1,7 +1,32 @@
 defmodule AdventOfCode.Day01 do
-  def part1(_args) do
+  def part1(args) do
+    chunks =
+      String.trim(args)
+      |> String.split("\n")
+
+    first_chunks = chunks |> Enum.map(&String.split(&1, " ")) |> Enum.map(&hd(&1)) |> Enum.sort()
+
+    second_chunks =
+      chunks |> Enum.map(&String.split(&1, " ")) |> Enum.map(&List.last(&1)) |> Enum.sort()
+
+    Enum.zip(first_chunks, second_chunks)
+    |> Enum.map(&abs(String.to_integer(elem(&1, 0)) - String.to_integer(elem(&1, 1))))
+    |> Enum.sum()
   end
 
-  def part2(_args) do
+  def part2(args) do
+    chunks =
+      String.trim(args)
+      |> String.split("\n")
+
+    first_chunks = chunks |> Enum.map(&String.split(&1, " ")) |> Enum.map(&hd(&1)) |> Enum.sort()
+
+    second_chunks =
+      chunks |> Enum.map(&String.split(&1, " ")) |> Enum.map(&List.last(&1)) |> Enum.sort()
+
+    Enum.map(first_chunks, fn a ->
+      Enum.count(second_chunks, fn b -> b == a end) * String.to_integer(a)
+    end)
+    |> Enum.sum()
   end
 end
--- test/advent_of_code/day_01_test.exs
+++ test/advent_of_code/day_01_test.exs
@@ -3,15 +3,36 @@ defmodule AdventOfCode.Day01Test do
 
   import AdventOfCode.Day01
 
+  @tag :skip
   test "part1" do
-    input = nil
+    input = AdventOfCode.Input.get!(1, 2024)
+
+    # input = """
+    # 3   4
+    # 4   3
+    # 2   5
+    # 1   3
+    # 3   9
+    # 3   3
+    # """
+
     result = part1(input)
 
-    assert result
+    assert result == 11
   end
 
   test "part2" do
-    input = nil
+    input = AdventOfCode.Input.get!(1, 2024)
+
+    # input = """
+    # 3   4
+    # 4   3
+    # 2   5
+    # 1   3
+    # 3   9
+    # 3   3
+    # """
+
     result = part2(input)
 
     assert result
